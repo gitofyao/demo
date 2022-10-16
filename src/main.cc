428723e5fcd4c2705fd708d556ea7113ea201e5f
gitofyao <1484051042@qq.com>
2022-10-16 20:08:50 +0800

    CMakeList.txt

--- src/main.cc
+++ src/main.cc
@@ -8,20 +8,18 @@ int main() {
     printf("Hello %d\n", 888);
 
     std::vector<int> vec = {1, 2, 3, 4, 5};
-    std::cout << "vec[2]: " << vec[2] << std::endl;
-    std::remove(vec.begin(), vec.end(), 3);
+    vec.clear();
     std::cout << "vec count: " << vec.size() << std::endl;
-    std::cout << "vec[2]: " << vec[2] << std::endl;
-    std::cout << "vec[4]: " << vec[4] << std::endl;
+    vec.emplace_back(1);
     for (const auto& n : vec) {
         std::cout << n << std::endl;
     }
     std::cout << "--------------------" << std::endl;
-    auto iter = std::find(vec.begin(), vec.end(), 3);
-    if (iter != vec.end()) {
-        vec.erase(iter);
-    }
-    for (const auto& n : vec) {
-        std::cout << n << std::endl;
-    }
+    // auto iter = std::find(vec.begin(), vec.end(), 3);
+    // if (iter != vec.end()) {
+    //     vec.erase(iter);
+    // }
+    // for (const auto& n : vec) {
+    //     std::cout << n << std::endl;
+    // }
 }
